c0da2e13d92ad338975f4a8ed0643e351c9d7825
Rishi-Rana1 <Rishirana123@outlook.com>
2023-06-30 03:59:47 -0400

    Added rps functionality

---
+++ rp.js
@@ -0,0 +1,54 @@
+function getComputerChoice() {
+    let chance = Math.random() * 100;
+    if (chance <= 33) {
+        let choice = "Rock"
+    }
+
+    else if (chance >= 33 && chance <= 66) {
+        let choice = "Paper"
+    }
+
+    else {
+        let choice = "Scissors"
+    }
+    return choice
+};
+
+function playRound (ComputerChoice, PlayerChoice) {
+    if (ComputerChoice === "Rock" && PlayerChoice === "Paper") {
+        return "You Win, Paper beats Rock!"
+    }
+
+    else if (ComputerChoice === "Paper" && PlayerChoice === "Rock") {
+        return "You Lose, Paper beats Rock!"
+}
+    else if (ComputerChoice === "Rock" && PlayerChoice === "Rock") {
+        return "Tie"
+    }
+    else if (ComputerChoice === "Paper" && PlayerChoice === "Scissors") {
+        return "You Win, Scissors beats Paper!"
+    }
+
+    else if (ComputerChoice === "Scissors" && PlayerChoice === "Paper") {
+        return "You Lose, Scissors beats Paper!"
+}
+    else if (ComputerChoice === "Scissors" && PlayerChoice === "Scissors") {
+        return "Tie"
+    }
+    else if (ComputerChoice === "Scissors" && PlayerChoice === "Rock") {
+        return "You Win, Rock beats Scissors!"
+    }
+
+    else if (ComputerChoice === "Rock" && PlayerChoice === "Scissors") {
+        return "You Lose, Rock beats Scissors!"
+}
+    else if (ComputerChoice === "Paper" && PlayerChoice === "Paper") {
+        return "Tie"
+    }
+};
+
+function game() {
+    for (let x = 0; x < 5; x++) {
+        let player = prompt("Pick Rock, Paper, or Scissors")
+    }
+}
